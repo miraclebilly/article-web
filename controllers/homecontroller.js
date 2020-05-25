bfdf3168be63689091fb15a9b1d894fe4e0c1dc5
Miracle Billy <uniquemiraculous@gmail.com>
2020-05-25 03:39:55 +0100

    Comment added

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,5 +1,6 @@
 const path          = require('path');
 const Article       = require('../models/write-article');
+const Comment       = require('../models/comment'); 
 
 
 exports.getArticles = (req, res, next) => {
@@ -24,17 +25,38 @@ exports.postArticle = (req, res, next) => {
         })
         .catch(err => {
          console.log(err);
-     })               
+     })  
+                
 }
 
 exports.getArticle = (req, res, next) => {
     res.render('new.ejs');
 }
 
+exports.postComment = (req, res, next) => {
+    const comment = new Comment(req.body);
+    comment.save()
+    .then(comment => {
+        return Article.findById(req.params.id)
+    })
+    .then(article => {
+        article.comments.unshift(comment);
+        return article.save();
+    })
+    .then(article => {
+        res.redirect('/home/'+ article._id)
+    })
+    
+    .catch(err => {
+        console.log(err)
+    })
+}
+
 exports.showArticle = (req, res, next) => {
     //show details of a single article
-    Article.findById(req.params.id)
+    Article.findById(req.params.id).populate("comments").exec()
     .then(result => {
+
         res.render('show', {article: result}); 
     })
     .catch(err => {
